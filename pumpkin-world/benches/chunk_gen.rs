867dc63ce23a80d98bfc927a0f1a378bc8d585ec
Martijn Voordouw <martijnvoordouw@gmail.com>
2025-06-14 10:29:46 +0200

    Added multiple seeds to chunk generation benchmark

--- pumpkin-world/benches/chunk_gen.rs
+++ pumpkin-world/benches/chunk_gen.rs
@@ -29,25 +29,31 @@ impl BlockRegistryExt for BlockRegistry {
     }
 }
 
-fn bench_chunk_generation(c: &mut Criterion) {
-    let seed = 0;
-    let generator = get_world_gen(Seed(seed), Dimension::Overworld);
+async fn chunk_generation_seed(seed: i64) {
+    let generator = get_world_gen(Seed(seed as u64), Dimension::Overworld);
     let temp_dir = TempDir::new().unwrap();
     let block_registry = Arc::new(BlockRegistry);
     let level = Arc::new(Level::from_root_folder(
         temp_dir.path().to_path_buf(),
         block_registry.clone(),
-        seed.try_into().unwrap(),
+        seed,
         Dimension::Overworld,
     ));
     let x = 0;
     let y = 0;
     let position = Vector2::new(x, y);
+    generator.generate_chunk(&level, block_registry.as_ref(), &position).await;
+}
+
+fn bench_chunk_generation(c: &mut Criterion) {
+    let seeds = [0, 42, 120, 200, 1000, 5000];
     let runtime = Runtime::new().unwrap();
-    c.bench_function("chunk generation", |b| {
-        b.to_async(&runtime)
-            .iter(|| generator.generate_chunk(&level, block_registry.as_ref(), &position))
-    });
+    for seed in seeds {
+        let name = format!("chunk generation seed {seed}");
+        c.bench_function(&name, |b| {
+            b.to_async(&runtime) .iter(|| chunk_generation_seed(seed))
+        });
+    }
 }
 
 criterion_group!(benches, bench_chunk_generation);
