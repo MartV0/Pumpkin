659edfc106a61c53633cc5b5ba8e5ee556f94178
Martijn Voordouw <martijnvoordouw@gmail.com>
2025-06-14 12:56:44 +0200

    Optimized block get_tag_values using phf

--- pumpkin-protocol/src/client/config/update_tags.rs
+++ pumpkin-protocol/src/client/config/update_tags.rs
@@ -36,7 +36,7 @@ impl ClientPacket for CUpdateTags<'_> {
                 WritingError::Message(format!("{} isn't representable as a VarInt", values.len()))
             })?)?;
 
-            for (key, values) in values.iter() {
+            for (key, values) in values.entries() {
                 // This is technically an `Identifier` but same thing
                 p.write_string_bounded(key, u16::MAX as usize)?;
                 p.write_list(values, |p, string_id| {
